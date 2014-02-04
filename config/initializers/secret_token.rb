79eeaed985e6c1e23ba21d7a0814de67fe8ad9fb
Faizal Zakaria <phaibusiness@gmail.com>
2014-02-05 00:21:47 +0800

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwesomeTest::Application.config.secret_token = '89a9e0b6b34f96a79c4109ca07a94fa99d980868d8c3900d94adc47a50a7d7dbd42fefef99d82c78c06fbef1a4293df4332ecbd32f1406e0687f4c8c7946336c'
